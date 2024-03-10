611e4bc58a18f19d8c525a8c2294e9cf9e2cf820
TranNguyetCan <Cantngcc210146@fpt.edu.vn>
2024-03-10 13:59:15 +0700

    about views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -36,11 +36,17 @@ public class HomeController : Controller
         return View();
         // return Redirect("~/Home/Cordinator");    
     }
+    public IActionResult About()
+    {
+        return View();
+       
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    
 
 }
