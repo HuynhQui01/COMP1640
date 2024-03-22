4c60e3b7f8a2d450b297b4aaca5253605cac1d72
thanhhao1903 <lyhao792@gmail.com>
2024-03-22 09:31:57 +0700

    update something for project

--- Models/CustomUser.cs
+++ Models/CustomUser.cs
@@ -6,11 +6,14 @@ namespace Comp1640.Models;
 public class CustomUser : IdentityUser
 {
     // public int FacId { get; set; }
-    public string? FacName {get; set; }
+    public string? FacName { get; set; }
     [InverseProperty("User")]
     public virtual ICollection<Contribution> Contributions { get; } = new List<Contribution>();
 
-    public string ProfileImagePath { get; set; }
+    [InverseProperty("User")]
+    public virtual ICollection<Feedback> Feedbacks { get; } = new List<Feedback>();
+
+    public string? ProfileImagePath { get; set; }
 
     // [ForeignKey("FacId")]
     // [InverseProperty("Users")]
--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -27,4 +27,8 @@ public partial class Feedback
     [ForeignKey("ConId")]
     [InverseProperty("Feedbacks")]
     public virtual Contribution? Con { get; set; }
+
+    [ForeignKey("UserId")]
+    [InverseProperty("Feedbacks")]
+    public virtual CustomUser? User { get; set; }
 }
