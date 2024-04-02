d2a55d570ccf4808e98dc8d1de884926fa4cfa88
Huỳnh Quí <huynhqui309@gmail.com>
2024-04-02 15:28:35 +0700

    check day feedback

--- Controllers/ConFeeController.cs
+++ Controllers/ConFeeController.cs
@@ -43,41 +43,46 @@ namespace Comp1640.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("FeedbackId, Comment, Fbtime, UserId, ConId")] Feedback feedback, int id)
         {
-            if (User.IsInRole("Student"))
+            var con = await _context.Contributions.FindAsync(id);
+            var conTime = con.SubmitDate;
+            var curTime = DateTime.Now;
+            var checkTime = (curTime - conTime).TotalDays;
+            if (checkTime <= 14)
             {
-                var userId = _userManager.GetUserId(User);
+                if (User.IsInRole("Student"))
+                {
+                    var userId = _userManager.GetUserId(User);
 
-                feedback.Fbtime = DateTime.Now;
-                feedback.ConId = id;
-                feedback.UserId = userId;
-                _context.Add(feedback);
-                await _context.SaveChangesAsync();
-                string content = "Your feedback has been reply at contribution ";
-                var message = new Message(new string[] { "quihvgcc210153@fpt.edu.vn" }, "Contribution feedback", content);
-                _emailSender.SendEmail(message);
-                return RedirectToAction("Details", new { id = id });
+                    feedback.Fbtime = DateTime.Now;
+                    feedback.ConId = id;
+                    feedback.UserId = userId;
+                    _context.Add(feedback);
+                    await _context.SaveChangesAsync();
+                    string content = "Your feedback has been reply at contribution ";
+                    var message = new Message(new string[] { "quihvgcc210153@fpt.edu.vn" }, "Contribution feedback", content);
+                    _emailSender.SendEmail(message);
+                    return RedirectToAction("Details", new { id = id });
+                }
+                if (User.IsInRole("Coordinator"))
+                {
+                    var userId = _userManager.GetUserId(User);
+                    var userName = _userManager.GetUserName(User);
+                    var user = _userManager.FindByIdAsync(feedback.UserId);
 
-
-            }
-            if (User.IsInRole("Coordinator"))
-            {
-                var userId = _userManager.GetUserId(User);
-                var userName = _userManager.GetUserName(User);
-                var user = _userManager.FindByIdAsync(feedback.UserId);
-
-                List<string> email = new List<string>();
-                email.Add(userName + "@gmail.com");
-                feedback.ConId = id;
-                feedback.UserId = _userManager.GetUserId(User);
-                _context.Add(feedback);
-                await _context.SaveChangesAsync();
-                string content = "Your contribution has been feedbacked at contribution ";
-                var message = new Message(email, "Contribution feedback", content);
-                return RedirectToAction("Details", new { id = id });
-
+                    List<string> email = new List<string>();
+                    email.Add(userName + "@gmail.com");
+                    feedback.ConId = id;
+                    feedback.UserId = _userManager.GetUserId(User);
+                    _context.Add(feedback);
+                    await _context.SaveChangesAsync();
+                    string content = "Your contribution has been feedbacked at contribution ";
+                    var message = new Message(email, "Contribution feedback", content);
+                    return RedirectToAction("Details", new { id = id });
+                }
+            }else{
+                TempData["ErrorMessage"] = "You can only provide feedback within 14 days after submission.";
             }
             return View("/Contribution/Index");
-
         }
 
         public async Task<IActionResult> Details(int id)
